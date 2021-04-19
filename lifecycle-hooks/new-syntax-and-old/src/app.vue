ea700740edcddd02a9b76e6d449e0924a41178b0
Caio Vinicius <caio.vinnicius2k@gmail.com>
2021-04-18 21:31:07 -0300

    important concepts

---
+++ lifecycle-hooks/new-syntax-and-old/src/app.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Lifecycle Hooks</h1>
+</template>
+
+<script>
+import {onMounted} from 'vue'
+
+// Hooks em composition API:
+export default {
+  
+  //Função Hook
+  setup () {
+    onMounted(() => {
+      console.log("Foi montado composition API")
+    })
+  }
+  
+}
+
+
+// Hooks em options API:
+// export default {
+  //Iniciando métodos e lifecycle hooks
+
+  //Hook mounted
+//   mouted () {
+//     console.log('Foi montado options API')
+//   }
+
+// }
+
+</script>
+
+<style>
+
+</style>
--- new_syntax/new-syntax-and-old/src/app.vue
+++ new_syntax/new-syntax-and-old/src/app.vue
@@ -1,96 +1,92 @@
 <template>
   <div>
     <h1>Minha lista de tarefas</h1>
-    <!-- Vai disparar ao clicar -->
     <button @click="handleShowList()">Ver a Lista!</button>
     <br />
-    <!-- Criando diretiva propria -->
 
     <input 
     type="text" 
     v-focus 
-    v-model="currentTask"
+    v-model="state.currentTask"
     @keyup.enter="addTask"
     >
 
-    <ul v-if="showList">
-      <!-- Com o v-if não irá aparecer a lista pois está como false -->
-      <!-- v-for é uma diretiva de iteração -->
-      <li v-for="(task, index) in tasks"
+    <ul v-if="state.showList">
+      <li v-for="(task, index) in state.tasks"
        @dblclick="complete(task)"
        :class="{ 'line-through': task.isDone }"
        class="task-item"
        :key="`${task}-${index}`">
-       <!-- O valor não pode se repetir, como não tem index, fazemos assim -->
-        <!-- Registrando um evento de duplo clique -->
-        <!-- v-bind para colocar a classe line-through quando a task for true -->
         {{ task.name }}
-        <!-- método remove passando um param task que estou percorrendo no v-for -->
         <button @click="remove(task)">&times;</button>
-        <!-- Ao rodar vai aparecer: "Fazer o curso" -->
-
       </li>
     </ul>
-    <!-- Se eu quiser a condição oposta ao v-if anterior: -->
     <p v-else>Lista de tarefas está escondida, clique no botão!</p>
   </div>
 </template>
 
 <script>
-// Criando diretiva propria
+import { reactive } from 'vue'
+
+// Componente
 const focus = {
-  // Quando a diretiva é inserida, recebo alguns params (el = element)
   inserted: (el) => {
     el.focus();
   },
 };
 
-// Componente:
 export default {
-  // Registrando diretivas
   directives: {
     focus,
   },
-
-  data: () => ({
+  setup () {
+    //Criando objeto reativo, cria um var state e nela chama a func reactive
+    //e dentro da função coloco o código do objeto que quero que se torne reativo
+    const state = reactive({
     currentTask: '',
     showList: false,
     tasks: [{ name: "Fazer o curso", isDone: false }],
-  }),
+  })
 
-  //Registrando métodos
-  methods: {
-    handleShowList() {
-      // para acessarmos um método dentro de um método, precisamos de this
-      this.showList = !this.showList;
-    },
-    complete(task) {
-      // Agora vou chamar o map, não o filter, pois não vou remover nada da lista
-      // (filtrar), vou ter a mesma list mas com o isDone falso ou true
-      this.tasks = this.tasks.map((t) => {
+  //Métodos:
+
+  function handleShowList() {
+      state.showList = !state.showList;
+    }
+
+    function complete(task) {
+      state.tasks = state.tasks.map((t) => {
         if (t.name === task.name) {
           return { ...t, isDone: !t.isDone };
         }
         return { ...t };
       });
-    },
-    remove(task) {
-      // Criando um array novo para remover, filtra o array coloca todo mundo de volta
-      // no array, menos o da comparação
-      this.tasks = this.tasks.filter((t) => t.name !== task.name);
+    }
+
+    function remove(task) {
+      state.tasks = state.tasks.filter((t) => t.name !== task.name);
       console.log("task", task);
-    },
-    addTask(){
-      //Tenho um array, colocar mais um item no array e depois zerar meu currentTask
-      //par ao input ficar vazio
-      this.tasks.push({
-        name: this.currentTask,
+    }
+
+    function addTask(){
+      state.tasks.push({
+        name: state.currentTask,
         isDone: false,
       })
-      this.currentTask = ''
+      state.currentTask = ''
     }
-  },
-};
+
+    return {
+      state,
+      //Retornando os métodos
+      handleShowList,
+      addTask,
+      complete,
+      remove,
+    }
+  }
+}
+
 </script>
 
 <style scoped>
